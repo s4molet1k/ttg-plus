7e8e0c7e369e2e9a3d552a37c204c8b7eb089257
wico. <107866650+s4molet1k@users.noreply.github.com>
2024-10-20 23:33:36 +0700

    why did this repo have the vanilla ttg

--- gamemodes/tacticaltoolgame/gamemode/weaponselector/config.lua
+++
@@ -1,6 +0,0 @@
-WeaponSelector = WeaponSelector or {}
-WeaponSelector.Colors = {
-    BG = Color(50, 50, 50),
-    Select = Color(65, 65, 65),
-    TextColor = Color(255, 255, 255), 
-}
--- gamemodes/tacticaltoolgame/gamemode/weaponselector_loader.lua
+++
@@ -1,7 +0,0 @@
-include("weaponselector/config.lua")
-if SERVER then
-    AddCSLuaFile("weaponselector/config.lua")
-    AddCSLuaFile("weaponselector/client/hooks.lua")
-elseif CLIENT then
-    include("weaponselector/client/hooks.lua")
-end
